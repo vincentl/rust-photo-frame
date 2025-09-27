ae1bb1f2436a7e8c5b8c1a03bece009f44bc45b0
vincentl <vincentl@users.noreply.github.com>
2025-09-26 22:35:50 -0400

    fix: correct WGSL struct syntax

--- src/tasks/greeting_screen.rs
+++ src/tasks/greeting_screen.rs
@@ -23,8 +23,8 @@ type AppConfig = crate::config::Configuration;
 
 const FRAME_SHADER: &str = r#"
 struct VertexOutput {
-    @builtin(position) position: vec4<f32>;
-    @location(0) color: vec4<f32>;
+    @builtin(position) position: vec4<f32>,
+    @location(0) color: vec4<f32>,
 };
 
 @vertex
