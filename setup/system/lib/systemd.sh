2e343aa650ca38b70e1678c936d2f01fe7be95f7
vincentl <vincentl@users.noreply.github.com>
2025-10-12 09:45:41 -0400

    Fix system systemd library shim

--- setup/system/lib/systemd.sh
+++ setup/system/lib/systemd.sh
@@ -1,1 +1,7 @@
-../../lib/systemd.sh
+#!/usr/bin/env bash
+# shellcheck shell=bash
+
+_system_lib_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck source=../../lib/systemd.sh
+source "${_system_lib_dir}/../../lib/systemd.sh"
+unset _system_lib_dir
